56fd1269d4de2a1625282efb26edca502eba3502
Natesh Narain <nnaraindev@gmail.com>
2022-07-05 00:04:46 +0000

    Use embedded hal traits

--- cansniffer/src/main.rs
+++ cansniffer/src/main.rs
@@ -5,9 +5,11 @@
 // @date Jul 04 2022
 //
 
+use anyhow::Context;
 use clap::Parser;
 
 use socketcan::CanSocket;
+use embedded_hal::can::{Can, Frame, Id};
 
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -25,9 +27,11 @@ struct Args {
 fn main() -> anyhow::Result<()> {
 
     let args = Args::parse();
+    let can_interface = args.interface;
 
-    let socket = CanSocket::open(&args.interface)?;
-    socket.set_nonblocking(true).expect("Failed to make socket non-blocking");
+    let mut socket = CanSocket::open(&can_interface)
+        .with_context(|| format!("Failed to open socket on interface {}", can_interface))?;
+    socket.set_nonblocking(true).with_context(|| format!("Failed to make socket non-blocking"))?;
 
     let shutdown = AtomicBool::new(false);
     let shutdown = Arc::new(shutdown);
@@ -39,11 +43,24 @@ fn main() -> anyhow::Result<()> {
     .expect("Failed to set signal handler");
 
     while !shutdown.load(Ordering::Relaxed) {
-        match socket.read_frame() {
-            Ok(frame) => println!("{:?}", frame),
+        match socket.receive() {
+            Ok(frame) => println!("{}", frame_to_string(&frame)),
             Err(_) => {},
         }
     }
 
     Ok(())
+}
+
+fn frame_to_string<F: Frame>(f: &F) -> String {
+    let id = {
+        match f.id() {
+            Id::Standard(id) => id.as_raw() as u32,
+            Id::Extended(id) => id.as_raw(),
+        }
+    };
+
+    let data_string = f.data().iter().fold(String::from(""), |a, b| format!("{} {:02x}", a, b));
+
+    format!("{:08X}  [{}] {}", id, f.dlc(), data_string)
 }
